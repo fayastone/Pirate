71afa1d2969d0be13dff869a4d4abec63f39fa5b
Julien Leveau <oursgroumy@gmail.com>
2015-12-29 13:50:27 +0100

    bundle game + ajouté des balises dans le html, pour structuré. rien de bien mechant

--- src/PA/CoreBundle/PACoreBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PA\CoreBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PACoreBundle extends Bundle
-{
-}
--- src/PA/UserBundle/PAUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PA\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PAUserBundle extends Bundle
-{
-}
